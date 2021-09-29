fe8315c248b914f378fa3815fc164d2d30356333
Chinwer <qiuyh0924@163.com>
2021-09-30 00:49:57 +0800

    400-500 subject ranking finished(No. 426 not finished)

--- spider.py
+++ spider.py
@@ -467,7 +467,7 @@ def init_driver() -> WebDriver:
 
 def main():
     browser = init_driver()
-    urls = get_all_urls_from_file('urls.txt')[300:400]
+    urls = get_all_urls_from_file('urls.txt')[400:500]
     unis = get_all_universities(urls, browser)
     save_to_excel(unis, 'res_new.xlsx')
 
